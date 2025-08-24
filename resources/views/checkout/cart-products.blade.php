5713b2e3f9a25440d541fc5de0b1de5c3cec7317
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 18:45:22 -0300

    lista de gateways

--- resources/views/checkout/cart-products.blade.php
+++ resources/views/checkout/cart-products.blade.php
@@ -1,7 +1,13 @@
 <div class="space-y-4">
     @foreach ($this->items as $item)
         <div class="flex items-center space-x-4 border p-3 rounded-lg shadow-sm bg-white">
-            <img src="{{ $item["img"] ?? "https://static.vecteezy.com/system/resources/previews/028/047/017/non_2x/3d-check-product-free-png.png" }}" alt="{{ $item["name"] }}" class="w-16 h-16 object-cover rounded-md">
+            <img
+                src="{{ $item["img"] ??
+                        "https://static.vecteezy.com/system/resources/previews/028/047/017/non_2x/3d-check-product-free-png.png" }}"
+                alt="{{ $item["name"] }}"
+                class="w-16 h-16 object-cover rounded-md"
+                style="margin-right: 20px;border-right: 1px dashed #cecece"
+            >
 
             <div class="flex flex-col">
                 <span class="font-semibold text-gray-800">{{ $item["name"] }}</span>
