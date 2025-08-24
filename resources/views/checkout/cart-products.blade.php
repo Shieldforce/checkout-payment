87c32ee10cade1d82318ba2f47eb93b222fef579
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 18:38:10 -0300

    lista de gateways

--- resources/views/checkout/cart-products.blade.php
+++ resources/views/checkout/cart-products.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-4">
     @foreach ($this->items as $item)
         <div class="flex items-center space-x-4 border p-3 rounded-lg shadow-sm bg-white">
-            <img src="{{ $item["img"] ?? asset("vendor/shieldforce/checkout-payment/src/Files/not-products-image.png") }}" alt="{{ $item["name"] }}" class="w-16 h-16 object-cover rounded-md">
+            <img src="{{ $item["img"] ?? "https://radio93fm.com.br/wp-content/uploads/2019/02/produto-585x380.png" }}" alt="{{ $item["name"] }}" class="w-16 h-16 object-cover rounded-md">
 
             <div class="flex flex-col">
                 <span class="font-semibold text-gray-800">{{ $item["name"] }}</span>
