832900ce3a5941f4de8c426c6b7fef70691f5b25
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-23 21:50:46 -0300

    criando pagina externo de checkout

--- src/Livewire/ExternalCheckoutPaymentPage.php
+++ src/Livewire/ExternalCheckoutPaymentPage.php
@@ -6,5 +6,5 @@ use Filament\Pages\SimplePage;
 
 class ExternalCheckoutPaymentPage extends SimplePage
 {
-    protected static string $view = "livewire.external-checkout-payment-page";
+    protected static string $view = "checkout-payment::livewire.external-checkout-payment-page";
 }
