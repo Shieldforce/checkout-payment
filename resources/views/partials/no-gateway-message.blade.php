f88f3f59cd994126ea9465fa8da6ab10121514d7
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 04:31:08 -0300

    lista de gateways

--- resources/views/partials/no-gateway-message.blade.php
+++ resources/views/partials/no-gateway-message.blade.php
@@ -5,14 +5,17 @@
     <p class="mt-2 text-gray-600">
         Para usar o checkout, você precisa cadastrar um gateway de pagamento.
     </p>
-    @php
-        $routeName = \Shieldforce\CheckoutPayment\Pages\CPPGatewaysPage::getRouteName()
-    @endphp
-    <br>
-    <hr>
-    <br>
-    <a href="{{ route( $routeName ) }}"
-       class="mt-4 inline-block px-4 py-2 bg-primary-600 text-white rounded-lg hover:bg-primary-700">
-        Ir para lista de gateways
-    </a>
+
+    @if(auth()->check())
+        @php
+            $routeName = \Shieldforce\CheckoutPayment\Pages\CPPGatewaysPage::getRouteName()
+        @endphp
+        <br>
+        <hr>
+        <br>
+        <a href="{{ route( $routeName ) }}"
+           class="mt-4 inline-block px-4 py-2 bg-primary-600 text-white rounded-lg hover:bg-primary-700">
+            Ir para lista de gateways
+        </a>
+    @endif
 </div>
