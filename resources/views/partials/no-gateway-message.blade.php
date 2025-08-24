161286f81ad67262413dcf2686105506bdd7b8d1
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 04:28:50 -0300

    lista de gateways

--- resources/views/partials/no-gateway-message.blade.php
+++ resources/views/partials/no-gateway-message.blade.php
@@ -8,7 +8,9 @@
     @php
         $routeName = \Shieldforce\CheckoutPayment\Pages\CPPGatewaysPage::getRouteName()
     @endphp
+    <br>
     <hr>
+    <br>
     <a href="{{ route( $routeName ) }}"
        class="mt-4 inline-block px-4 py-2 bg-primary-600 text-white rounded-lg hover:bg-primary-700">
         Ir para lista de gateways
