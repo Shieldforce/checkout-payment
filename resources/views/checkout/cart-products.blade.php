897e038393f04f5fedc64cacd849589ff59df889
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 18:32:46 -0300

    lista de gateways

--- resources/views/checkout/cart-products.blade.php
+++ resources/views/checkout/cart-products.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-4">
     @foreach ($this->items as $item)
         <div class="flex items-center space-x-4 border p-3 rounded-lg shadow-sm bg-white">
-            <img src="{{ $item["img"] ?? "../../../src/Files/not-products-image.png" }}" alt="{{ $item["name"] }}" class="w-16 h-16 object-cover rounded-md">
+            <img src="{{ $item["img"] ?? base_path("src/Files/not-products-image.png") }}" alt="{{ $item["name"] }}" class="w-16 h-16 object-cover rounded-md">
 
             <div class="flex flex-col">
                 <span class="font-semibold text-gray-800">{{ $item["name"] }}</span>
