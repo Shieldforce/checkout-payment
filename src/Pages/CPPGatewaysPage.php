62f19a4a7b4a858d8796eaee3f666836c413faaf
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 00:58:40 -0300

    lista de gateways

--- src/Pages/CPPGatewaysPage.php
+++ src/Pages/CPPGatewaysPage.php
@@ -111,7 +111,7 @@ class CPPGatewaysPage extends Page implements HasForms, HasTable
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make('add')
+            Actions\Action::make('add')
                 ->label('Adicionar')
                 ->form([
                     TextInput::make('name')->required(),
