5609d42286107e5b6ef7866d3a40cfc1a7e58582
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 15:16:23 -0300

    lista de gateways

---
+++ src/Services/MountCheckoutStepsService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Shieldforce\CheckoutPayment\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Shieldforce\CheckoutPayment\Enums\MethodPaymentEnum;
+use Shieldforce\CheckoutPayment\Models\CppCheckout;
+use Shieldforce\CheckoutPayment\Models\CppGateways;
+
+class MountCheckoutStepsService
+{
+    public function __construct(
+        protected Model $model,
+        protected array $requiredMethods = [
+            MethodPaymentEnum::credit_card->value,
+            MethodPaymentEnum::pix->value,
+            MethodPaymentEnum::billet->value,
+        ]
+    ) {}
+
+    public function handle()
+    {
+        // Gateway Ativo ---
+        $cppGateway = CppGateways::where('active', true)->first();
+
+        if (isset($cppGateway->id)) {
+
+            $cppCheckout = CppCheckout::updateOrCreate([
+                "cpp_gateway_id"    => $cppGateway->id,
+                "referencable_id"   => $this->model->id,
+                "referencable_type" => $this->model::class,
+                "methods"           => json_encode($this->requiredMethods),
+            ], []);
+
+            // Step 1 -> Items do checkout ---
+            /*$productsMounts = array_map(function ($product) {
+                return [
+                    'name'        => $product["name"],
+                    'price'       => $product["pivot"]["price"],
+                    'price_2'     => $product["pivot"]["price"],
+                    'price_3'     => $product["pivot"]["price"],
+                    'description' => "Venda de produto: " . $product["name"],
+                    'img'         => $product["picture"],
+                    'quantity'    => $product["pivot"]["quantity"],
+                ];
+            }, $model->order->products->toArray());
+            $cppCheckout->step1()->updateOrCreate([
+                "cpp_checkout_id" => $cppCheckout->id,
+            ], [
+                'items'   => json_encode($productsMounts),
+                'visible' => true,
+            ]);*/
+
+            // Step 2 -> Dados do cliente ---
+            /*$nameExplode = explode(" ", trim($model->order->client->name));
+            $firstName   = $nameExplode[0];
+            $lastName    = $nameExplode[1] ?? "-";
+            $contact     = $model->order->client->contacts()->first();
+            $prefix      = $contact->prefix ?? null;
+            $number      = $contact->number ?? null;
+            $phoneNumber = $prefix . $number;
+            $cppCheckout->step2()->updateOrCreate([
+                "cpp_checkout_id" => $cppCheckout->id,
+            ], [
+                "first_name"   => $firstName,
+                "last_name"    => $lastName,
+                "email"        => $model->order->client->email,
+                "phone_number" => $phoneNumber,
+                "document"     => $model->order->client->document,
+                'visible'      => true,
+            ]);*/
+
+            // Step 3 -> Endereço do cliente ---
+            /*$address = $model->order->client->addresses()->where("main", 1)->first();
+            $cppCheckout->step3()->updateOrCreate([
+                "cpp_checkout_id" => $cppCheckout->id,
+            ], [
+                "zipcode"    => $address->zipcode,
+                "street"     => $address->street,
+                "district"   => $address->district,
+                "city"       => $address->city,
+                "state"      => $address->state,
+                "number"     => $address->number,
+                "complement" => $address->complement,
+                'visible'    => true,
+            ]);*/
+        }
+    }
+
+}
