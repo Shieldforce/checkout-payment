dbe324600f25aff65e0158569fb84fed5d442b2a
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 20:44:22 -0300

    lista de gateways

--- src/Pages/InternalCheckoutWizard.php
+++ src/Pages/InternalCheckoutWizard.php
@@ -135,7 +135,7 @@ class InternalCheckoutWizard extends Page implements HasForms
                         'checkout-payment::checkout.cart-products'
                     ),
                 ]),
-            Wizard\Step::make('Dados Pessoais')
+            /*Wizard\Step::make('Dados Pessoais')
                 ->visible($this->step2->visible ?? true)
                 ->schema([
 
@@ -299,7 +299,7 @@ class InternalCheckoutWizard extends Page implements HasForms
                             return $options;
                         })
                         ->required(),
-                ]),
+                ]),*/
             Wizard\Step::make('Confirmação')
                 ->schema([
                     TextInput::make('review')->default('Revisar seus dados')->disabled(),
