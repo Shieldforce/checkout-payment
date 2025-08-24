ca80f4517de0c9ede3f58fe23e93c95da5ec45ba
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-24 17:52:18 -0300

    lista de gateways

--- src/Pages/InternalCheckoutWizard.php
+++ src/Pages/InternalCheckoutWizard.php
@@ -66,7 +66,7 @@ class InternalCheckoutWizard extends Page implements HasForms
         $this->email = request()->query('email') ?? null;
         $this->name  = request()->query('first_name') ?? null;
 
-        $this->items = json_decode($this?->checkout?->step1()?->first()?->items ?? []);
+        dd($this?->checkout?->step1()?->first()?->items);
 
         $this->form->fill();
     }
