20aa61f0b7f45e10f41026eec007ab1fa33a84af
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-25 12:37:14 -0300

    lista de gateways

--- resources/views/pages/internal-checkout-wizard.blade.php
+++ resources/views/pages/internal-checkout-wizard.blade.php
@@ -109,6 +109,7 @@
                 // Inicializa quando a aba de cartão for visível
                 Livewire.hook('message.processed', (message, component) => {
                     const methodChecked = component.get('method_checked');
+                    console.log(methodChecked);
                     if (methodChecked === {{ \Shieldforce\CheckoutPayment\Enums\MethodPaymentEnum::credit_card->value }}) {
                         if (!cardForm) {
                             cardForm = initCardForm();
