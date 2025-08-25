db3d230cd3b686dc28a928617aa3d831d1b29554
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-25 18:04:44 -0300

    lista de gateways

--- resources/views/pages/internal-checkout-wizard.blade.php
+++ resources/views/pages/internal-checkout-wizard.blade.php
@@ -10,91 +10,6 @@
         <script>
 
             document.addEventListener('DOMContentLoaded', async () => {
-
-                // verifica se os campos existem
-                const form = document.getElementById('form-checkout-wizard');
-                const cardNumber = document.getElementById('cardNumber');
-                const expiration = document.getElementById('cardExpiration');
-                const cvv = document.getElementById('cardCVV');
-                const holder = document.getElementById('cardholderName');
-                const email = document.getElementById('email');
-                const installments = document.getElementById('installments');
-                const issuer = document.getElementById('issuer');
-
-                if (!form || !cardNumber || !expiration || !cvv || !holder || !email || !installments || !issuer) {
-                    console.log('Campos do cartão ainda não renderizados');
-                    return null;
-                }
-
-                return mp.cardForm({
-                    amount: '100.00',
-                    autoMount: true,
-                    form: {
-                        id: 'form-checkout-wizard',
-                        cardNumber: {
-                            id: "cardNumber",
-                            placeholder: '0000 0000 0000 0000',
-                        },
-                        expirationDate: {
-                            id: 'cardExpiration',
-                            placeholder: 'mm/YY',
-                        },
-                        securityCode: {
-                            id: 'cardCVV',
-                            placeholder: '345'
-                        },
-                        cardholderName: {
-                            id: 'cardholderName',
-                            placeholder: 'Fulano da Silva'
-                        },
-                        email: { id: 'email' },
-                        installments: {
-                            id: 'installments',
-                            placeholder: 'Quantidade de parcelas'
-                        },
-                        issuer: {
-                            id: 'issuer',
-                            placeholder: 'Tipo de cartão'
-                        },
-                    },
-                    callbacks: {
-                        onFormMounted: error => {
-                            if (error) return console.warn("Form Mounted handling error: ", error);
-                            console.log("Form mounted");
-                        },
-                        /*onSubmit: function(event) {
-                            /!*event.preventDefault();
-                            const formData = cardForm.getCardFormData();
-                            console.log('Token gerado:', formData.token);*!/
-                            // @this.call('processarPagamentoCartao', formData.token)
-                        },
-                        onInstallmentsReceived: function(error, data) {
-                            console.log("onInstallmentsReceived", data);
-                        },
-                        onIssuersReceived: function(error, data) {
-                            console.log("onIssuersReceived", data);
-                        },*/
-                        /*onBinChange: function (data) {
-                            console.log("onBinChange:", data);
-
-                            // só muda se o BIN realmente for diferente
-                            if (data && data.bin && data.bin !== lastBin) {
-                                lastBin = data.bin;
-                                console.log("Novo BIN detectado:", data.bin);
-                            } else {
-                                // ignora, evita resetar o installments
-                                console.log("Ignorado, BIN não mudou de fato");
-                            }
-                        },*/
-                        onError: function(error) {
-                            console.log("error:", error);
-                        }
-                    },
-                });
-
-            })
-
-            /*document.addEventListener('DOMContentLoaded', async () => {
                 const mp = new window.MercadoPago("{{ \Illuminate\Support\Facades\Crypt::decrypt($cppGateways->field_1) }}", {
                     locale: "pt-BR",
                 });
@@ -154,7 +69,7 @@
                                     if (error) return console.warn("Form Mounted handling error: ", error);
                                     console.log("Form mounted");
                                 },
-                                /!*onSubmit: function(event) {
+                                /*onSubmit: function(event) {
                                     /!*event.preventDefault();
                                     const formData = cardForm.getCardFormData();
                                     console.log('Token gerado:', formData.token);*!/
@@ -165,8 +80,8 @@
                                 },
                                 onIssuersReceived: function(error, data) {
                                     console.log("onIssuersReceived", data);
-                                },*!/
-                                /!*onBinChange: function (data) {
+                                },*/
+                                /*onBinChange: function (data) {
                                     console.log("onBinChange:", data);
 
                                     // só muda se o BIN realmente for diferente
@@ -177,7 +92,7 @@
                                         // ignora, evita resetar o installments
                                         console.log("Ignorado, BIN não mudou de fato");
                                     }
-                                },*!/
+                                },*/
                                 onError: function(error) {
                                     console.log("error:", error);
                                 }
@@ -198,12 +113,12 @@
                     if (valueSelectMethodCheck === creditCardEnum) {
                         if (!cardForm) {
                             setTimeout(function() {
-                                cardForm = initCardForm();
-                            }, 2000)
+                                //cardForm = initCardForm();
+                            }, 3000)
                         }
                     }
                 })
-            });*/
+            });
         </script>
     @endpush
 @endif
