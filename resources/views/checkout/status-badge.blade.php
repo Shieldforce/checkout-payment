64acefa8305b5a9a73ecf2a2f1a1f3894bec22e1
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-26 12:02:31 -0300

    lista de gateways

--- resources/views/checkout/status-badge.blade.php
+++ resources/views/checkout/status-badge.blade.php
@@ -1,3 +1,3 @@
-<span class="px-2 py-1 rounded-full text-white {{ $checkout->status === 2 ? 'bg-green-600' : 'bg-yellow-600' }}">
-    {{ $checkout->status === 2 ? 'Aprovado' : 'Em Processamento' }}
+<span class="px-2 py-1 rounded-full text-white {{ $this->checkout->status === 2 ? 'bg-green-600' : 'bg-yellow-600' }}">
+    {{ $this->checkout->status === 2 ? 'Aprovado' : 'Em Processamento' }}
 </span>
