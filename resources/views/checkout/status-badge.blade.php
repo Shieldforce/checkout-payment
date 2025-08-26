96f88988a1b911cbcc8c777c1669eb8020a69b15
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-26 14:13:23 -0300

    lista de gateways

--- resources/views/checkout/status-badge.blade.php
+++ resources/views/checkout/status-badge.blade.php
@@ -1,6 +1,6 @@
 <div>
     <span id="statusLabel" class="px-3 py-1 rounded bg-gray-200">
-        {{ $this->status }}
+        {{ $this->statusCheckout }}
     </span>
 
     {{-- Atualiza sozinho a cada 30s --}}
