6cdec2991756203b61cbd2fb65e511dcb3b87d4c
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-26 18:30:25 -0300

    lista de gateways

--- resources/views/checkout/pix-preview.blade.php
+++ resources/views/checkout/pix-preview.blade.php
@@ -5,9 +5,9 @@
         first_name: @entangle('first_name').live,
         last_name: @entangle('last_name').live,
     }"
-    class="relative w-full max-w-md h-[300px] rounded-xl shadow-2xl
+    class="relative w-full max-w-md h-[300px] rounded-xl shadow-lg
            bg-gradient-to-r from-green-400 via-green-500 to-green-600
-           text-white p-6 select-none flex flex-col justify-between items-center"
+           text-white p-4 select-none flex flex-col justify-between"
 >
     <!-- Header -->
     <div class="flex justify-between items-center w-full mb-2">
@@ -16,26 +16,26 @@
     </div>
 
     <!-- QR Code centralizado -->
-    <div class="flex-1 flex items-center justify-center w-full">
+    <div class="flex-1 flex items-center justify-center">
         <template x-if="base_qrcode">
             <img
                 :src="base_qrcode" alt="QR Code PIX"
-                class="w-40 h-40 sm:w-48 sm:h-48 object-contain rounded-lg border border-white/30 shadow-lg"
+                class="max-w-full max-h-[180px] object-contain rounded-lg border border-white/30 shadow-md"
             />
         </template>
         <template x-if="!base_qrcode">
-            <div class="w-40 h-40 sm:w-48 sm:h-48 bg-white/20 flex items-center justify-center rounded-lg border border-white/30 shadow-lg">
+            <div class="max-w-full max-h-[180px] bg-white/20 flex items-center justify-center rounded-lg border border-white/30 shadow-md">
                 <img
                     src="https://img.icons8.com/fluent/512/pix.png" alt="PIX Logo"
-                    class="w-24 h-24 object-contain"
+                    class="max-w-[120px] max-h-[120px] object-contain"
                 />
             </div>
         </template>
     </div>
 
     <!-- Rodapé -->
-    <div class="w-full flex justify-between items-center text-xs mt-4">
-        <div class="truncate max-w-[120px]">
+    <div class="w-full flex justify-between items-center text-xs mt-2">
+        <div class="truncate max-w-[60%]">
             <div class="opacity-80 text-[10px]">Nome do Pagador</div>
             <div class="font-semibold truncate" x-text="(first_name || 'Primeiro Nome') + ' ' + (last_name || 'Sobrenome')"></div>
         </div>
