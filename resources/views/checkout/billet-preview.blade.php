fdea5d5d1a0615ae9be462ea6d266515386716ee
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-27 00:13:59 -0300

    lista de gateways

--- resources/views/checkout/billet-preview.blade.php
+++ resources/views/checkout/billet-preview.blade.php
@@ -1,12 +1,12 @@
 <div class="flex flex-col items-center justify-center p-4">
-    @if($this->url_billet)
+    @if($this->url_billet ?? $this->step4->url_billet ?? null)
         <iframe
-            src="{{ $this->url_billet }}"
+            src="{{ $this->url_billet ?? $this->step4->url_billet ?? null }}"
             class="w-full h-[500px] rounded-lg border border-gray-300 dark:border-white/30 shadow-md">
         </iframe>
 
         <a
-            href="{{ $this->url_billet }}"
+            href="{{ $this->url_billet ?? $this->step4->url_billet ?? null }}"
             target="_blank"
             class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">
             Abrir boleto em nova aba
