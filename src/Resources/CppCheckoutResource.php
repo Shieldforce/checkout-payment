5d51ed1e20de991575aaa624af8c3a85be2f3768
Alexandre Ferreira <shieldforce2@gmail.com>
2025-08-27 15:07:08 -0300

    lista de checkouts

--- src/Resources/CppCheckoutResource.php
+++ src/Resources/CppCheckoutResource.php
@@ -49,8 +49,7 @@ class CppCheckoutResource extends Resource
                         }
                         return collect($state)
                             ->map(function ($item) {
-                                $msg = "<span class='inline-flex items-center px-2 py-0.5 rounded-full";
-                                $msg .= "text-xs font-medium bg-green-100 text-green-800 mr-1 mb-1'>{$item}</span>";
+                                $msg = "<span class='inline-flex items-center px-2 py-0.5 rounded-fulltext-xs font-medium bg-green-100 text-green-800 mr-1 mb-1'>{$item}</span>";
                                 return $msg;
                             })
                             ->join('');
