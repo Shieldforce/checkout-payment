2d9aaad12ec0e53e1df462b8525a9c23d5a6ad13
Alexandre Ferreira <shieldforce2@gmail.com>
2025-09-04 09:53:30 -0300

    preço

--- src/Pages/InternalCheckoutWizard.php
+++ src/Pages/InternalCheckoutWizard.php
@@ -909,6 +909,8 @@ class InternalCheckoutWizard extends Page implements HasForms
 
                     $pdf = $return["transaction_details"]["external_resource_url"] ?? $return["pdf"];
 
+                    dd($return["transaction_details"]["external_resource_url"]);
+
                     $this->checkout->step4()->updateOrCreate([
                         "cpp_checkout_id" => $this->checkout->id,
                     ], [
