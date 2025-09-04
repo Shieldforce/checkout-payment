fef452b30d5728df29739ffede7ea221b17a13c7
Alexandre Ferreira <shieldforce2@gmail.com>
2025-09-04 10:07:52 -0300

    preço

--- resources/views/checkout/billet-preview.blade.php
+++ resources/views/checkout/billet-preview.blade.php
@@ -1,9 +1,18 @@
 <div class="flex flex-col items-center justify-center">
-    @if($this->url_billet ?? $this->step4->url_billet ?? null)
-        <iframe
+    @php
+        $billetUrl = $this->url_billet ?? $this->step4->url_billet ?? null;
+    @endphp
+
+    @if($billetUrl)
+        {{--<iframe
             src="{{ $this->url_billet ?? $this->step4->url_billet ?? null }}"
             class="w-full min-h-screen rounded-lg border border-gray-300 dark:border-white/30 shadow-md">
-        </iframe>
+        </iframe>--}}
+
+        <iframe
+            src="https://docs.google.com/viewer?embedded=true&url={{ urlencode($billetUrl) }}"
+            class="w-full min-h-screen rounded-lg border border-gray-300 dark:border-white/30 shadow-md"
+        ></iframe>
     @else
         <img
             src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!f305cw"
