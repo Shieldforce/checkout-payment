fbd2c09153ea117caca621fc612589b1024b9dba
Alexandre Ferreira <shieldforce2@gmail.com>
2025-09-04 09:32:23 -0300

    preço

--- resources/views/checkout/cart-products.blade.php
+++ resources/views/checkout/cart-products.blade.php
@@ -1,6 +1,6 @@
+{{--
 <div class="space-y-4">
     @if(isset($this->items))
-        @dd($this->items)
         @foreach ($this->items as $item)
             <div class="flex items-center space-x-4 border p-3 rounded-lg shadow-sm bg-white">
                 <img
@@ -18,6 +18,42 @@
                     R$ {{ number_format($item["price"], 2, ',', '.') }}
                 </span>
                     <span class="text-xs text-gray-500">Qtd: {{ $item["quantity"] }}</span>
+                </div>
+            </div>
+        @endforeach
+    @endif
+</div>
+--}}
+
+<div class="space-y-4">
+    @if(isset($this->items))
+        @dd($this->items)
+        @foreach ($this->items as $item)
+            <div
+                class="flex items-center space-x-4 border p-3 rounded-lg shadow-sm
+                       bg-white dark:bg-gray-800
+                       border-gray-200 dark:border-gray-700"
+            >
+                <img
+                    src="{{ $item["img"] ??
+                        "https://static.vecteezy.com/system/resources/previews/028/047/017/non_2x/3d-check-product-free-png.png" }}"
+                    alt="{{ $item["name"] }}"
+                    class="w-16 h-16 object-cover rounded-md pr-2 mr-2 border-r border-dashed border-gray-300 dark:border-gray-600"
+                >
+
+                <div class="flex flex-col">
+                    <span class="font-semibold text-gray-800 dark:text-gray-100">
+                        {{ $item["name"] }}
+                    </span>
+                    <span class="text-sm text-gray-600 dark:text-gray-300">
+                        {{ $item["description"] ?? 'Sem descrição' }}
+                    </span>
+                    <span class="text-sm text-green-600 dark:text-green-400 font-bold">
+                        R$ {{ number_format($item["price"], 2, ',', '.') }}
+                    </span>
+                    <span class="text-xs text-gray-500 dark:text-gray-400">
+                        Qtd: {{ $item["quantity"] }}
+                    </span>
                 </div>
             </div>
         @endforeach
