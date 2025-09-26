46407ac39fcc9e9a57f4bde06a13f31eacbbbebe
Shieldforce <17112341+Shieldforce@users.noreply.github.com>
2025-09-26 18:30:40 +0000

    Fix styling

--- src/Services/DtoSteps/DtoStep2.php
+++ src/Services/DtoSteps/DtoStep2.php
@@ -8,24 +8,24 @@ class DtoStep2
 {
     public function __construct(
         public TypePeopleEnum $people_type,
-        public string         $first_name,
-        public string         $last_name,
-        public string         $email,
-        public int            $phone_number,
-        public string         $document,
-        public ?bool          $visible = null,
+        public string $first_name,
+        public string $last_name,
+        public string $email,
+        public int $phone_number,
+        public string $document,
+        public ?bool $visible = null,
     ) {}
 
     public function toArray(): array
     {
         return [
-            'people_type'  => $this->people_type,
-            'first_name'   => $this->first_name,
-            'last_name'    => $this->last_name,
-            'email'        => $this->email,
+            'people_type' => $this->people_type,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
             'phone_number' => $this->phone_number,
-            'document'     => $this->document,
-            'visible'      => $this->visible,
+            'document' => $this->document,
+            'visible' => $this->visible,
         ];
     }
 }
