ac8388a74b9e8f05816668ee3bb46453ca69cfa0
Shieldforce <17112341+Shieldforce@users.noreply.github.com>
2025-10-09 15:30:24 +0000

    Fix styling

--- src/Resources/CppCheckoutResource/Pages/ListCppCheckouts.php
+++ src/Resources/CppCheckoutResource/Pages/ListCppCheckouts.php
@@ -13,7 +13,7 @@ class ListCppCheckouts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            //Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
