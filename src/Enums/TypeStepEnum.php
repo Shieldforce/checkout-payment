d562a489b1a59a144445e1b214748922644a3467
Shieldforce <17112341+Shieldforce@users.noreply.github.com>
2025-12-05 23:53:02 +0000

    Fix styling

--- src/Enums/TypeStepEnum.php
+++ src/Enums/TypeStepEnum.php
@@ -4,20 +4,20 @@ namespace Shieldforce\CheckoutPayment\Enums;
 
 enum TypeStepEnum: int
 {
-    case items          = 1;
+    case items = 1;
     case dados_pessoais = 2;
     case dados_endereco = 3;
-    case pagamento      = 4;
-    case finalizado     = 5;
+    case pagamento = 4;
+    case finalizado = 5;
 
     public function label(): string
     {
         return match ($this) {
-            self::items          => 'Carrinho',
+            self::items => 'Carrinho',
             self::dados_pessoais => 'Dados Pessoais',
             self::dados_endereco => 'Dados de Endereco',
-            self::pagamento      => 'Pagamento',
-            self::finalizado     => 'Finalizado',
+            self::pagamento => 'Pagamento',
+            self::finalizado => 'Finalizado',
         };
     }
 }
--- src/Rules/ImageUrlRule.php
+++ src/Rules/ImageUrlRule.php
@@ -35,4 +35,3 @@ class ImageUrlRule implements ValidationRule
         }*/
     }
 }
-
