99a182924b9f1e8a68ebd413b1242db8f29b74af
Alexandre Ferreira <shieldforce2@gmail.com>
2026-01-06 21:46:53 -0300

    day

--- src/Resources/CppCheckoutResource/Pages/ListCppCheckouts.php
+++ src/Resources/CppCheckoutResource/Pages/ListCppCheckouts.php
@@ -17,6 +17,8 @@ class ListCppCheckouts extends ListRecords
 
     protected function getHeaderActions(): array
     {
+        $year = now()->year;
+
         return [
             Actions\Action::make('runMonthlyBilling')
                 ->label('Rodar Faturamento Mensal')
@@ -25,19 +27,31 @@ class ListCppCheckouts extends ListRecords
 
                 // 👉 FORM DO MODAL
                 ->form([
-                    TextInput::make('reference')
+                    /*TextInput::make('reference')
                         ->label('Referência')
                         ->helperText('Ex: 01/2026')
                         ->default(now()->subMonth()->format('m/Y'))
-                        ->required(),
-
-                    /*TextInput::make('billingDay')
-                        ->label('Dia de Cobrança')
-                        ->numeric()
-                        ->minValue(1)
-                        ->maxValue(31)
-                        ->default(now()->format("d"))
                         ->required(),*/
+
+                    Select::make('reference')
+                        ->label('Referência')
+                        ->required()
+                        ->options([
+                            "01/{$year}" => "01/{$year} - Janeiro",
+                            "02/{$year}" => "02/{$year} - Fevereiro",
+                            "03/{$year}" => "03/{$year} - Março",
+                            "04/{$year}" => "04/{$year} - Abril",
+                            "05/{$year}" => "05/{$year} - Maio",
+                            "06/{$year}" => "06/{$year} - Junho",
+                            "07/{$year}" => "07/{$year} - Julho",
+                            "08/{$year}" => "08/{$year} - Agosto",
+                            "09/{$year}" => "09/{$year} - Setembro",
+                            "10/{$year}" => "10/{$year} - Outubro",
+                            "11/{$year}" => "11/{$year} - Novembro",
+                            "12/{$year}" => "12/{$year} - Dezembro",
+                        ])
+                        ->default(now()->subMonth()->format('m'))
+                        ->live(),
 
                     Select::make('billingDay')
                         ->label('Dia de Cobrança')
