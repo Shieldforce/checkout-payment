d7b49bfba4e678c12f2faacc5d7dab63e6dcb3f8
Alexandre Ferreira <shieldforce2@gmail.com>
2026-06-06 11:07:47 -0300

    implementando sicoob

--- src/Pages/InternalCheckoutWizard.php
+++ src/Pages/InternalCheckoutWizard.php
@@ -910,6 +910,8 @@ class InternalCheckoutWizard extends Page implements HasForms
                     "senha_certificado" => $firstGatewaySicoob->field_1,
                 ]);
 
+                dd($login);
+
                 $dueDate = Carbon::parse($transaction->due_date)
                     ->format('Y-m-d');
 
