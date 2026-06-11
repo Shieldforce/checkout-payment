4bc11d62a52de2ee161a653a1033b431d59d4eb4
Shieldforce <17112341+Shieldforce@users.noreply.github.com>
2026-06-11 15:28:03 +0000

    Fix styling

--- src/Resources/CppCheckoutResource.php
+++ src/Resources/CppCheckoutResource.php
@@ -92,7 +92,7 @@ class CppCheckoutResource extends Resource
                     ->description('Métodos liberados')
                     ->formatStateUsing(function ($state) {
                         $array = json_decode($state, true);
-                        $tags  = [];
+                        $tags = [];
                         foreach ($array as $key => $value) {
                             $tags[] = MethodPaymentEnum::from($value)->label();
                         }
@@ -116,13 +116,13 @@ class CppCheckoutResource extends Resource
                     }),
 
                 BadgeColumn::make('status')
-                    ->formatStateUsing(fn($state, $record) => StatusCheckoutEnum::labelEnum($state))
-                    ->color(fn($state, $record) => StatusCheckoutEnum::colorEnum($state))
+                    ->formatStateUsing(fn ($state, $record) => StatusCheckoutEnum::labelEnum($state))
+                    ->color(fn ($state, $record) => StatusCheckoutEnum::colorEnum($state))
                     ->label('Status')
                     ->sortable(),
 
                 BadgeColumn::make('startOnStep')
-                    ->formatStateUsing(fn($state, $record) => TypeStepEnum::from($state)->label())
+                    ->formatStateUsing(fn ($state, $record) => TypeStepEnum::from($state)->label())
                     ->color('success')
                     ->label('Passo Atual')
                     ->sortable(),
@@ -144,7 +144,7 @@ class CppCheckoutResource extends Resource
                             ->toArray()
                     )
                     ->query(function ($query, array $data) {
-                        if (!empty($data['value'])) {
+                        if (! empty($data['value'])) {
                             $query->whereHas('step2', function ($subQuery) use ($data) {
                                 $subQuery->where('document', $data['value']);
                             });
@@ -165,7 +165,7 @@ class CppCheckoutResource extends Resource
                             ->toArray()
                     )
                     ->query(function ($query, array $data) {
-                        if (!empty($data['value'])) {
+                        if (! empty($data['value'])) {
                             $query->whereHas('step2', function ($subQuery) use ($data) {
                                 $subQuery->where('email', $data['value']);
                             });
@@ -186,7 +186,7 @@ class CppCheckoutResource extends Resource
                             ->toArray()
                     )
                     ->query(function ($query, array $data) {
-                        if (!empty($data['value'])) {
+                        if (! empty($data['value'])) {
                             $query->whereHas('step2', function ($subQuery) use ($data) {
                                 $subQuery->where('first_name', $data['value']);
                             });
@@ -221,7 +221,7 @@ class CppCheckoutResource extends Resource
                     ->label('Status')
                     ->options(StatusCheckoutEnum::options())
                     ->query(
-                        fn(Builder $query, array $data) => filled($data['value'])
+                        fn (Builder $query, array $data) => filled($data['value'])
                             ? $query->where('status', $data['value'])
                             : $query
                     ),
@@ -236,7 +236,7 @@ class CppCheckoutResource extends Resource
                     // Tables\Actions\EditAction::make(),
 
                     Tables\Actions\DeleteAction::make()
-                        ->visible(fn($record) => $record->status == StatusCheckoutEnum::criado->value),
+                        ->visible(fn ($record) => $record->status == StatusCheckoutEnum::criado->value),
 
                     Tables\Actions\Action::make('Link de Pagamento')
                         ->icon('heroicon-o-credit-card')
@@ -253,12 +253,12 @@ class CppCheckoutResource extends Resource
                         ->modalCancelActionLabel('Fechar')
                         ->modalContent(function (Model $record) {
                             if ($record->gateway->name == TypeGatewayEnum::mercado_pago->value) {
-                                $mps        = new MercadoPagoService;
+                                $mps = new MercadoPagoService;
                                 $pagamentos = $mps->buscarPagamentoPorExternalId($record->uuid);
 
                                 logger([
                                     'external' => $record->uuid ?? null,
-                                    'return'   => $pagamentos ?? null,
+                                    'return' => $pagamentos ?? null,
                                 ]);
 
                                 $approved = collect($pagamentos)
@@ -285,13 +285,13 @@ class CppCheckoutResource extends Resource
 
                                 return view('checkout-payment::partials.pagamento-mp', [
                                     'pagamentos' => $pagamentos,
-                                    'record'     => $record,
+                                    'record' => $record,
                                 ]);
                             }
 
                             if ($record->gateway->name == TypeGatewayEnum::sicoob->value) {
-                                $boletoPixSicoob = new BoletoPixService();
-                                $consultar       = $boletoPixSicoob->consult($record);
+                                $boletoPixSicoob = new BoletoPixService;
+                                $consultar = $boletoPixSicoob->consult($record);
                                 logger($consultar);
                             }
 
